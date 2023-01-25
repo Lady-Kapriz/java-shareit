2c6887e67d1ba18e191cee67ab20bcadef8be21c
Mariya Semenova <99213891+Lady-Kapriz@users.noreply.github.com>
2023-01-26 01:25:48 +0400

    устранена ошибка

--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.repository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.UserNotFoundException;
-import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.Collection;
