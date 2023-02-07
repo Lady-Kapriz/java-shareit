229624f466f10bfe877736037bcebc9283dc144c
Mariya Semenova <99213891+Lady-Kapriz@users.noreply.github.com>
2023-02-08 00:56:25 +0400

    sprint 14, v9

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.mapper;
 
 import lombok.experimental.UtilityClass;
-import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.dto.BookingCreateDto;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.mapper;
 
 import lombok.experimental.UtilityClass;
-import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.dto.BookingDtoForItem;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.CommentDto;
